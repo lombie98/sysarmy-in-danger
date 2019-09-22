b2c290e310fd5cd0ff4027eb833ffc82dd3acfdc
Martin Nicolas Cuesta <cuesta.martin.n@hotmail.com>
2019-09-22 10:58:31 -0300

    [YES] First Release (Arduino Code)

--- ArduinoReceiver/ArduinoReceiver.ino
+++ ArduinoReceiver/ArduinoReceiver.ino
@@ -1,7 +1,7 @@
 #include <TimerOne.h>
 #include <Wire.h>
 
-#define maxInputs 64
+#define maxInputs 32
 #define COMMAND_LENGTH 128
 #define SLAVE_ADDRESS 0x04
 
@@ -9,19 +9,22 @@ int timer_count = 0;
 int TIMER_NANOSECONDS = 500;
 
 int inputsQty;
-
 bool inputConnState[maxInputs];
-// Channels should be received from the RasPi and synced with other Arduino
 int inputChannels[maxInputs]; 
-// Ins
 int inputs[maxInputs];
 int testing_inputs[8] = {22, 24, 26, 28, 30, 32, 34, 36};
 
+// Arduino Mega PinMap (Leftmost 32 pin cluster)
+int inputArray[maxInputs] = {
+  22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52,
+  23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53
+};
+
 struct state_struct {
-  int Low;
-  int High;
-  int LastLow;
-  int LastHigh;
+    int Low;
+    int High;
+    int LastLow;
+    int LastHigh;
 };
 // States should match the amount of pins, configurable from te RasPi
 state_struct inputStates[maxInputs];
@@ -34,236 +37,197 @@ char stateString[maxInputs] = {'\0'};
 int statusCounter = 0;
 
 void setup() {
-  //pinMode(LED_BUILTIN, OUTPUT);
-  //digitalWrite(LED_BUILTIN, LOW);
+    
+    Wire.begin(SLAVE_ADDRESS);
+    Wire.setClock(400000);
+    Wire.onReceive(receiveData);
+    Wire.onRequest(sendData);
 
-  
-  // start serial for output
-  // Serial.begin(1000000);
-  // Serial.println("Program Started");
-
-  Wire.begin(SLAVE_ADDRESS);
-  Wire.setClock(400000);
-  Wire.onReceive(receiveData);
-  Wire.onRequest(sendData);
-
-  // Init Timer
-  Timer1.initialize(TIMER_NANOSECONDS);
-  Timer1.attachInterrupt(timer1_callback);
+    // Init Timer
+    Timer1.initialize(TIMER_NANOSECONDS);
+    Timer1.attachInterrupt(timer1_callback);
 }
+
 void loop() {
-  delay(100);
-  if (qty_done && pos_array_done && inputsQty) {
-    for (int i = 0; i < inputsQty; i++) {
-      if (inputConnState[i]) {
-        stateString[i] = '1';
-      } else {
-        stateString[i] = '0';
-      }
+    delay(100);
+    if (game_started) {
+        for (int i = 0; i < inputsQty; i++) {
+            if (inputConnState[i]) {
+                stateString[i] = '1';
+            } else {
+                stateString[i] = '0';
+            }
+        }
+        stateString[inputsQty] = '\0';
     }
-    stateString[inputsQty] = '\0';
-  }
 }
 
 void start_game() {
-  // Testing HardCoded
-  for (int i = 0; i < inputsQty; i++) {
-    inputs[i] = testing_inputs[i];
-    inputChannels[i] = (i * 10) + 100;
-  }
+    // Testing HardCoded
+    for (int i = 0; i < inputsQty; i++) {
+        // inputs[i] = testing_inputs[i];
+        inputChannels[i] = (i * 10) + 100;
+    }
 
-  // Set Inputs as IN
-  for (int i = 0; i < inputsQty; i++) {
-    inputStates[i] = {0, 0, 0, 0};
-    inputConnState[i] = false;
-    int in = inputs[i];
-    pinMode(in, INPUT_PULLUP);
-  }
+    // Set Inputs as IN
+    for (int i = 0; i < inputsQty; i++) {
+        inputStates[i] = {0, 0, 0, 0};
+        inputConnState[i] = false;
+        int in = inputs[i];
+        pinMode(in, INPUT_PULLUP);
+    }
 
-  game_started = true;
-
-  /*
-  Serial.println("Game Config:");
-  Serial.print("Input Qty: "); Serial.println(inputsQty);
-  Serial.print("Channels: ");
-  for (int i = 0; i < inputsQty; i++) {
-    Serial.print(inputChannels[i]); Serial.print(", ");
-  }
-  Serial.println("");
-  Serial.print("Input Pins: ");
-  for (int i = 0; i < inputsQty; i++) {
-    Serial.print(inputs[i]); Serial.print(", ");
-  }
-  Serial.println("");
-  */
-  
+    game_started = true;
 }
-
 
 void sendData() {
-  if (qty_done && pos_array_done) {
-    Wire.write(stateString[statusCounter]);
-    statusCounter++;
-    if (statusCounter > inputsQty) {
-      statusCounter = 0;
-    }
-  } else {
-    Wire.write('E');
-  }
-}
-
-
-void receiveData(int byteCount) {
-  char command[COMMAND_LENGTH] = {'\0'};
-
-  recvWithStartEndMarkers('$', ';', command);
-  if (command[0] != '\0') {
-    _process_received_command(command);
-  }
-
-}
-
-void recvWithStartEndMarkers(char startMarker, char endMarker, char *receivedChars) {
-  static boolean recvInProgress = false;
-  static byte ndx = 0;
-  char rc;
-  boolean newData = false;
-  int retry = 0;
-
-  while (!newData && retry < 1000) {
-    retry ++;
-    if (Wire.available() > 0) {
-      rc = Wire.read();
-
-      if (recvInProgress == true) {
-        if (rc != endMarker) {
-          receivedChars[ndx] = rc;
-          ndx++;
-          if (ndx >= COMMAND_LENGTH) {
-            ndx = COMMAND_LENGTH - 1;
-          }
+    if (game_started) {
+        Wire.write(stateString[statusCounter]);
+        statusCounter++;
+        if (statusCounter > inputsQty) {
+            statusCounter = 0;
         }
-        else {
-          receivedChars[ndx] = '\0'; // terminate the string
-          recvInProgress = false;
-          ndx = 0;
-          newData = true;
-        }
-      }
-
-      else if (rc == startMarker) {
-        recvInProgress = true;
-      }
+    } else {
+        Wire.write('E');
     }
-  }
 }
 
 void _process_received_command(char *command) {
-  int pos_array[maxInputs];
+    int pos_array[maxInputs];
 
-  int pos_index;
+    int pos_index;
 
-  char * strtokIndx; // this is used by strtok() as an index
-  char directive[16];
-  strtokIndx = strtok(command, ":");      // get the first part - the directive
-  strcpy(directive, strtokIndx); // copy it to messageFromPC
-  if (!strcmp(directive, "status")) {
-    statusCounter = 0;
-  }
-  if (!strcmp(directive, "conn_qty")) {
-    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
-    inputsQty = atoi(strtokIndx);
-    qty_done = true;
-  }
-  else if (!strcmp(directive, "pos")) {
-    strtokIndx = strtok(NULL, ",");
-    pos_index = atoi(strtokIndx);
-    pos_array_done = true;
-  }
+    char * strtokIndx; // this is used by strtok() as an index
+    char directive[16];
+    strtokIndx = strtok(command, ":"); // get the first part - the directive
+    strcpy(directive, strtokIndx); // copy it to messageFromPC
+
+    if (!strcmp(directive, "status")) {
+        statusCounter = 0;
+    }
+    else if (!strcmp(directive, "restart")) {
+        qty_done = false;
+        game_started = false;
+        pos_array_done = false;
+    }
+    else if (!strcmp(directive, "conn_qty")) {
+        strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+        inputsQty = atoi(strtokIndx);
+        qty_done = true;
+    }
+    else if (!strcmp(directive, "pos")) {
+        int ii = 0;
+        while( strtokIndx != NULL && ii < maxInputs ) {
+          strtokIndx = strtok(NULL, ",");
+          inputs[ii] = inputArray[atoi(strtokIndx)];
+          ii++;
+        }
+        pos_array_done = true;
+    }
 }
 
 void check_connections() {
-  bool result;
-  for (int i = 0; i < inputsQty; i++) {
-    result = _match_input(inputs[i], inputStates[i], inputChannels[i]);
-    inputConnState[i] = result;
-  }
+    bool result;
+    for (int i = 0; i < inputsQty; i++) {
+        result = _match_input(inputs[i], inputStates[i], inputChannels[i]);
+        inputConnState[i] = result;
+    }
 }
 
 bool _match_input(int pin, state_struct &state, int ticks) {
-  bool result = false;
-  int pinRead = digitalRead(pin);
-  if (pinRead) {
-    if (!state.High) {
-      state.LastLow = state.Low;
-      state.Low = 0;
+    bool result = false;
+    int pinRead = digitalRead(pin);
+    if (pinRead) {
+        if (!state.High) {
+            state.LastLow = state.Low;
+            state.Low = 0;
+        }
+        state.High ++;
+    } else {
+        if (!state.Low) {
+            state.LastHigh = state.High;
+            state.High = 0;
+        }
+        state.Low ++;
     }
-    state.High ++;
-  } else {
-    if (!state.Low) {
-      state.LastHigh = state.High;
-      state.High = 0;
+    if (state.High > 2000) {
+        state.High = 0; 
     }
-    state.Low ++;
-  }
-  if (state.High > 2000) {
-    state.High = 0; 
-  }
-  if (state.Low > 2000) {
-    state.Low = 0;
-  }
-  /*
-  Serial.print(state.Low);
-  Serial.print(" ,");
-  Serial.println(state.High);
-  */
-  if (ticks - 2 <= state.LastHigh && state.LastHigh <= ticks + 2 &&
-      ticks - 2 <= state.LastLow && state.LastLow <= ticks + 2 &&
-      state.High < ticks + 5 && state.Low < ticks + 5) {
-    // digitalWrite(LED_BUILTIN, HIGH);
-    result = true;
-  } else {
-    // digitalWrite(LED_BUILTIN, LOW);
-    result = false;
-  }
-  return result;
-}
-
-
-void send_status() {
-  if (timer_count % 2000 == 0) {
-    Serial.println("Status:");
-    for (int i = 0; i < inputsQty; i++) {
-      Serial.print("Input: "); Serial.print(inputs[i]);
-      if (inputConnState[i]) {
-        Serial.println(" is properly connected");
-      } else {
-        Serial.println(" is not connected");
-      }
+    if (state.Low > 2000) {
+        state.Low = 0;
     }
-  }
+    if (ticks - 2 <= state.LastHigh && state.LastHigh <= ticks + 2 &&
+            ticks - 2 <= state.LastLow && state.LastLow <= ticks + 2 &&
+            state.High < ticks + 5 && state.Low < ticks + 5) {
+        result = true;
+    } else {
+        result = false;
+    }
+    return result;
 }
 
 void timer1_callback() {
 
-  if ((! game_started) && qty_done && pos_array_done) {
-    start_game();
-  }
-  /*
-  if (qty_done && pos_array_done && (timer_count % 60 == 0)) {
-    send_status();
-  }
-  */
-  check_connections();
+    if ((! game_started) && qty_done && pos_array_done) {
+        start_game();
+    }
+    if (game_started) {
+        check_connections();
+    }
 
-  // Timer Counter HealthCheck
-  prevent_timer_overflow();
+    // Timer Counter HealthCheck
+    prevent_timer_overflow();
+}
+
+// Same in both... Maybe a lib?
+void receiveData(int byteCount) {
+    char command[COMMAND_LENGTH] = {'\0'};
+
+    recvWithStartEndMarkers('$', ';', command);
+    if (command[0] != '\0') {
+        _process_received_command(command);
+    }
+}
+
+void recvWithStartEndMarkers(char startMarker, char endMarker, char *receivedChars) {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char rc;
+    boolean newData = false;
+    int retry = 0;
+
+    while (!newData && retry < 1000) {
+        retry ++;
+        if (Wire.available() > 0) {
+            rc = Wire.read();
+
+            if (recvInProgress == true) {
+                if (rc != endMarker) {
+                    receivedChars[ndx] = rc;
+                    ndx++;
+                    if (ndx >= COMMAND_LENGTH) {
+                        ndx = COMMAND_LENGTH - 1;
+                    }
+                }
+                else {
+                    receivedChars[ndx] = '\0'; // terminate the string
+                    recvInProgress = false;
+                    ndx = 0;
+                    newData = true;
+                }
+            }
+
+            else if (rc == startMarker) {
+                recvInProgress = true;
+            }
+        }
+    }
 }
 
 void prevent_timer_overflow() {
-  timer_count ++;
-  // Prevent Timer Counter Overflow
-  if (timer_count > 10000) {
-    timer_count = 0;
-  }
+    timer_count ++;
+    // Prevent Timer Counter Overflow
+    if (timer_count > 10000) {
+        timer_count = 0;
+    }
 }
--- ArduinoSender/ArduinoSender.ino
+++ ArduinoSender/ArduinoSender.ino
@@ -1,86 +1,111 @@
 #include <TimerOne.h>
 #include <Wire.h>
 
-#define maxOutputs 64
+#define maxOutputs 32
 #define COMMAND_LENGTH 128
 #define SLAVE_ADDRESS 0x05
 
 int timer_count = 0;
 int TIMER_NANOSECONDS = 500;
 
-int outputsQty;// = 8;
-
-// State
+int outputsQty;
 bool outputStates[maxOutputs];
-// Channels should be received from the RasPi and synced with other Arduino
 int outputChannels[maxOutputs];
-// OUTS
 int outputs[maxOutputs];
 int testing_outputs[8] = {22,24,26,28,30,32,34,36};
+
+// Arduino Mega PinMap (Leftmost 32 pin cluster)
+int outputArray[maxOutputs] = {
+  22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52,
+  23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53
+};
 
 bool qty_done = false;
 bool pos_array_done = false;
 bool game_started = false;
 
 void setup() {
-  // Serial.begin(1000000);
-  // Serial.println("Program Started");
+    Wire.begin(SLAVE_ADDRESS);
+    Wire.setClock(400000);
+    Wire.onReceive(receiveData);
 
-  Wire.begin(SLAVE_ADDRESS);
-  Wire.setClock(400000);
-  Wire.onReceive(receiveData);
-
-  // Init Timer
-  Timer1.initialize(TIMER_NANOSECONDS);
-  Timer1.attachInterrupt(timer1_callback);
-}
-
-void start_game() {
-  // Testing HardCoded
-  for (int i = 0; i < outputsQty; i++) {
-    outputs[i] = testing_outputs[i];
-    outputChannels[i] = (i * 10) + 100;
-  }
-
-  // Set Outputs as OUT
-  for (int i = 0; i < outputsQty; i++) {
-    outputStates[i] = false;
-    int out = outputs[i];
-    pinMode(out, OUTPUT);
-  }
-  game_started = true;
-  // Serial.println("Started");
-
-  /*
-  Serial.println("Game Config:");
-  Serial.print("Output Qty: "); Serial.println(outputsQty);
-  Serial.print("Channels: ");
-  for (int i = 0; i < outputsQty; i++) {
-    Serial.print(outputChannels[i]); Serial.print(", ");
-  }
-  Serial.println("");
-  Serial.print("Output Pins: ");
-  for (int i = 0; i < outputsQty; i++) {
-    Serial.print(outputs[i]); Serial.print(", ");
-  }
-  Serial.println("");
-  */
-
+    // Init Timer
+    Timer1.initialize(TIMER_NANOSECONDS);
+    Timer1.attachInterrupt(timer1_callback);
 }
 
 void loop() {
-  delay(100);
+    delay(100);
 }
 
+void start_game() {
+    // Testing HardCoded
+    for (int i = 0; i < outputsQty; i++) {
+        // outputs[i] = testing_outputs[i];
+        outputChannels[i] = (i * 10) + 100;
+    }
+
+    // Set Outputs as OUT
+    for (int i = 0; i < outputsQty; i++) {
+        outputStates[i] = false;
+        int out = outputs[i];
+        pinMode(out, OUTPUT);
+    }
+    game_started = true;
+}
+
+bool _process_received_command(char *command) {
+    char * strtokIndx; // this is used by strtok() as an index
+    char directive[16];
+    strtokIndx = strtok(command, ":"); // get the first part - the directive
+    strcpy(directive, strtokIndx); // copy it to messageFromPC
+    if (!strcmp(directive, "conn_qty")) {
+        strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+        outputsQty = atoi(strtokIndx);
+        qty_done = true;
+    }
+    else if (!strcmp(directive, "restart")) {
+        qty_done = false;
+        game_started = false;
+        pos_array_done = false;
+    }
+    else if (!strcmp(directive, "pos")) {
+        int ii = 0;
+        while( strtokIndx != NULL && ii < maxOutputs ) {
+          strtokIndx = strtok(NULL, ",");
+          outputs[ii] = outputArray[atoi(strtokIndx)];
+          ii++;
+        }
+        pos_array_done = true;
+    }
+}
+
+void make_connections() {
+    for (int i = 0; i < outputsQty; i++) {
+        if (timer_count % outputChannels[i] == 0) {
+            digitalWrite(outputs[i], outputStates[i]);
+            outputStates[i] = !outputStates[i];
+        }
+    }
+}
+
+void timer1_callback() {
+    if ((! game_started) && qty_done && pos_array_done) {
+        start_game();
+    }
+    make_connections();
+    // Timer Counter HealthCheck
+    prevent_timer_overflow();
+}
+
+// Same in both... Maybe a lib?
 void receiveData(int byteCount) {
-  char command[COMMAND_LENGTH] = {'\0'};
+    char command[COMMAND_LENGTH] = {'\0'};
 
-  recvWithStartEndMarkers('$', ';', command);
-  if (command[0] != '\0') {
-    // Serial.println(command);
-    _process_received_command(command);
-  }
-
+    recvWithStartEndMarkers('$', ';', command);
+    if (command[0] != '\0') {
+        _process_received_command(command);
+    }
 }
 
 void recvWithStartEndMarkers(char startMarker, char endMarker, char *receivedChars) {
@@ -90,69 +115,38 @@ void recvWithStartEndMarkers(char startMarker, char endMarker, char *receivedCha
     boolean newData = false;
     int retry = 0;
 
-
     while (!newData && retry < 1000) {
-    retry ++;
-    if (Wire.available() > 0) {
-        rc = Wire.read();
+        retry ++;
+        if (Wire.available() > 0) {
+            rc = Wire.read();
 
-        if (recvInProgress == true) {
-            if (rc != endMarker) {
-                receivedChars[ndx] = rc;
-                ndx++;
-                if (ndx >= COMMAND_LENGTH) {
-                    ndx = COMMAND_LENGTH - 1;
+            if (recvInProgress == true) {
+                if (rc != endMarker) {
+                    receivedChars[ndx] = rc;
+                    ndx++;
+                    if (ndx >= COMMAND_LENGTH) {
+                        ndx = COMMAND_LENGTH - 1;
+                    }
+                }
+                else {
+                    receivedChars[ndx] = '\0'; // terminate the string
+                    recvInProgress = false;
+                    ndx = 0;
+                    newData = true;
                 }
             }
-            else {
-                receivedChars[ndx] = '\0'; // terminate the string
-                recvInProgress = false;
-                ndx = 0;
-                newData = true;
+
+            else if (rc == startMarker) {
+                recvInProgress = true;
             }
         }
-
-        else if (rc == startMarker) {
-            recvInProgress = true;
-        }
     }
-    }
-}
-
-bool _process_received_command(char *command) {
-  char * strtokIndx; // this is used by strtok() as an index
-  char directive[16];
-  strtokIndx = strtok(command, ":");      // get the first part - the directive
-  strcpy(directive, strtokIndx); // copy it to messageFromPC
-  if (!strcmp(directive, "conn_qty")) {
-    // Serial.println("Received conn_qty command");
-    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
-    outputsQty = atoi(strtokIndx);
-    qty_done = true;
-    pos_array_done = true;
-  }
-}
-
-void timer1_callback() {
-  if ((! game_started) && qty_done && pos_array_done) {
-    start_game();
-  }
-  // The sending of the signals should be in a loop, configurable from the RasPi
-  for (int i = 0; i < outputsQty; i++) {
-    if (timer_count % outputChannels[i] == 0) {
-      // Serial.println("Sending");
-      digitalWrite(outputs[i], outputStates[i]);
-      outputStates[i] = !outputStates[i];
-    }
-  }
-  // Timer Counter HealthCheck
-  prevent_timer_overflow();
 }
 
 void prevent_timer_overflow() {
-  timer_count ++;
-  // Prevent Timer Counter Overflow
-  if (timer_count > 10000) {
-    timer_count = 0;
-  }
+    timer_count ++;
+    // Prevent Timer Counter Overflow
+    if (timer_count > 10000) {
+        timer_count = 0;
+    }
 }
